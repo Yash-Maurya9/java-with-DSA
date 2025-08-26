2b08d6b789f70ae3ce79614531441e62419f02e5
Yash Maurya <yashmaurya7097@gmail.com>
2025-08-26 16:08:09 +0530

    Add files via upload

---
+++ JavaLearning/first.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+class Student{
+public static void main(String[]args){
+    Scanner input = new Scanner(System.in);
+
+    System.out.println("Entre your first number");
+    
+    int num1 = input.nextInt();
+
+    System.out.println("Entre your second number");
+
+    int num2 = input.nextInt();
+
+    int sum = num1+num2;
+
+    System.out.println("sum" +sum);
+
+    System.out.println();
+
+    
+   
+}
+};
